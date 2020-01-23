5446567cb4fca26427f04eb83a72c1f8a726abcb
Tinku Kumar Singh <tinkukumarsingh03@gmail.com>
2020-01-23 14:50:52 +0530

    first commit

---
+++ database1.php
@@ -0,0 +1,17 @@
+<?php
+include('connect.php')
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+</head>
+<body>
+    <?php
+    $sql = "SELECT * from animals_details
+     from Animals_details
+      where Name = 'Tiger';
+
+    ?>
+</body>
+</html>
